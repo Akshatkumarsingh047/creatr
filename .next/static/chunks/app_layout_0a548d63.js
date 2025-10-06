fd29e7101b8c6ea96fb65f1850992c7720eb556d
akshatkumarsingh047 <akshatkumarsingh047@gmail.com>
2025-10-06 13:15:12 +0530

    initial commit

---
+++ .next/static/chunks/app_layout_0a548d63.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__f5cf3249._.css",
+  "static/chunks/_216b6474._.js"
+],
+    source: "dynamic"
+});
